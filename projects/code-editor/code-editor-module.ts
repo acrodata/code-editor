e1fd4373353bb7c0a7415284f1d0cce06ca2ca9f
nzbin <nzb329@163.com>
2024-05-24 13:57:10 +0800

    refactor: add code-editor module

---
+++ projects/code-editor/code-editor-module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CodeEditor } from './code-editor';
+
+@NgModule({
+  imports: [CodeEditor],
+  exports: [CodeEditor],
+})
+export class CodeEditorModule {}
