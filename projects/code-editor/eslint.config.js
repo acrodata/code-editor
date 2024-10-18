09342bdb422b9570945501c52a42746c6784b502
nzbin <nzb329@163.com>
2024-10-18 10:47:41 +0800

    chore(eslint): update rules

--- projects/code-editor/eslint.config.js
+++ projects/code-editor/eslint.config.js
@@ -23,8 +23,6 @@ module.exports = tseslint.config(
           style: 'kebab-case',
         },
       ],
-      '@angular-eslint/directive-class-suffix': 'off',
-      '@angular-eslint/component-class-suffix': 'off',
     },
   },
   {
