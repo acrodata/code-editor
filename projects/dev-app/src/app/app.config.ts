1e6f5ba2dcdc41ca3c38039f66b494afb559df77
nzbin <nzb329@163.com>
2025-12-10 20:46:06 +0800

    chore: remove unused angular dependencies

--- projects/dev-app/src/app/app.config.ts
+++ projects/dev-app/src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [provideRouter(routes)],
 };
