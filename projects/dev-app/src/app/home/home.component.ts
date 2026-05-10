cd5287da99ff018fe484290c09170053f887c8cf
nzbin <nzb329@163.com>
2026-05-10 17:29:22 +0800

    refactor(dev-app): use public folder

--- projects/dev-app/src/app/home/home.component.ts
+++ projects/dev-app/src/app/home/home.component.ts
@@ -108,7 +108,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   }
 
   getLangSample(lang: string) {
-    fetch(`assets/lang_samples/${lang}.txt`).then(async response => {
+    fetch(`lang_samples/${lang}.txt`).then(async response => {
       if (response.ok) {
         this.code = await response.text();
       } else {
